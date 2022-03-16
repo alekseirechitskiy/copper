7532b1b110a6ba14b6a2a61b6a17c5470c5bbc12
slowspirit <slowspirit@gmail.com>
2022-03-16 13:48:56 +0300

    added product-page page

--- src/js/modules/slickModule.js
+++ src/js/modules/slickModule.js
@@ -4,6 +4,25 @@ window.$ = $;
 import slick from 'slick-carousel';
 
 export default function () {
+
+    $('.slider-thumb').slick({
+        asNavFor: '.slider-big',
+        vertical: true,
+        slidesToShow: 5,
+        arrows: true,
+        focusOnSelect: true,
+        draggable: true,
+        variableWidth: true,
+        infinite: false,
+    });
+
+    $('.slider-big').slick({
+        asNavFor: '.slider-thumb',
+        arrows: false,
+        fade: true,
+
+    });
+
     $('.section-w-slider__slider').slick({
         infinite: true,
         slidesToShow: 3,
